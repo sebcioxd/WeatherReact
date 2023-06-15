e0971d4c5ea0ff1e4604733154c6a8de38d05ce0
Terb <103338510+TerbEnjoyer@users.noreply.github.com>
2023-06-15 21:19:05 +0200

    update reactdom

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// if you're using react18 then comments reactdom above and uncomment the code below.
+
 // import { createRoot } from 'react-dom/client';
 // const container = document.getElementById('root');
 // const root = createRoot(container); // createRoot(container!) if you use TypeScript
